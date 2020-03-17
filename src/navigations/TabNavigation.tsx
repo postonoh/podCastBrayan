03173435954838cc8f97712fd41f5deb6afd6d1c
postonoh <sposton@emscorporate.com>
2020-03-17 14:21:48 -0400

    Adding Icons

--- src/navigations/TabNavigation.tsx
+++ src/navigations/TabNavigation.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+
 import routes from './routes';
 import HomeScreen from '../components/home/HomeScreen';
 import ProfileScreen from '../components/profile/Profile';
@@ -12,7 +15,9 @@ const Tab = createBottomTabNavigator();
 const TabNavigation: React.FC = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name={routes.HOME} component={HomeScreen} />
+            <Tab.Screen options={{
+                tabBarIcon: () => <FeatherIcon name="inbox"size={30} />
+            }} name={routes.HOME} component={HomeScreen} />
             <Tab.Screen name={routes.PROFILE} component={ProfileScreen} />
             <Tab.Screen name={routes.LIBRARY} component={LibraryScreen} />
             <Tab.Screen name={routes.DOWNLOADS} component={DownloadsScreen} />

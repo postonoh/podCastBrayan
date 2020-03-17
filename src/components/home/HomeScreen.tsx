e57da7c2c6fe6950d3fc963f839378afbb924fde
postonoh <sposton@emscorporate.com>
2020-03-17 16:01:49 -0400

    Starting working on Home page adding layout for podcast

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Box, Text } from 'react-native-design-utility';
+import { ScrollView } from 'react-native-gesture-handler';
+import { theme } from '../../constants/theme';
 
 const HomeScreen: React.FC = () => {
     return (
-        <Box f={1} center>
-            <Text>HomeScreen</Text>
+        <Box f={1} center bg="white">
+            <Box mt="xl" >
+                <Box ml="sm" mb="sm">
+                    <Text size="xl" weight="bold">
+                        Trending
+                    </Text>
+                </Box>
+                <Box>
+                    <ScrollView contentContainerStyle={{ marginLeft: theme.space.sm }} horizontal showsHorizontalScrollIndicator={false}>
+                        <Box w={100} h={100} radius={4} bg="red" mr="sm"></Box>
+                        <Box w={100} h={100} radius={4} bg="red" mr="sm"></Box>
+                        <Box w={100} h={100} radius={4} bg="red" mr="sm"></Box>
+                        <Box w={100} h={100} radius={4} bg="red" mr="sm"></Box>
+                        <Box w={100} h={100} radius={4} bg="red" mr="sm"></Box>
+                    </ScrollView>
+                </Box>
+            </Box>
         </Box>
-
     );
 };
 
--- src/navigations/TabNavigation.tsx
+++ src/navigations/TabNavigation.tsx
@@ -8,19 +8,35 @@ import HomeScreen from '../components/home/HomeScreen';
 import ProfileScreen from '../components/profile/Profile';
 import LibraryScreen from '../components/library/LibraryScreen';
 import DownloadsScreen from '../components/downloads/DownloadsScreen';
+import { metrics } from '../constants/metrics';
+import { theme } from '../constants/theme';
+
 
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation: React.FC = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator tabBarOptions={{
+            activeTintColor: theme.color.green,
+            inactiveTintColor: theme.color.blueDarker,
+            showLabel: false
+        }}>
             <Tab.Screen options={{
-                tabBarIcon: () => <FeatherIcon name="inbox"size={30} />
+                tabBarIcon: ({ color: color }) => <FeatherIcon name="home" size={metrics.tabIconSize} color={color} />
             }} name={routes.HOME} component={HomeScreen} />
-            <Tab.Screen name={routes.PROFILE} component={ProfileScreen} />
-            <Tab.Screen name={routes.LIBRARY} component={LibraryScreen} />
-            <Tab.Screen name={routes.DOWNLOADS} component={DownloadsScreen} />
+            <Tab.Screen options={{
+                tabBarIcon: ({ color: color }) => <FeatherIcon name="user" size={metrics.tabIconSize} color={color} />
+            }}
+                name={routes.PROFILE} component={ProfileScreen} />
+            <Tab.Screen options={{
+                tabBarIcon: ({ color: color }) => <FeatherIcon name="inbox" size={metrics.tabIconSize} color={color} />
+            }}
+                name={routes.LIBRARY} component={LibraryScreen} />
+            <Tab.Screen options={{
+                tabBarIcon: ({ color: color }) => <FeatherIcon name="headphones" size={metrics.tabIconSize} color={color} />
+            }}
+                name={routes.DOWNLOADS} component={DownloadsScreen} />
         </Tab.Navigator>
     );
 };

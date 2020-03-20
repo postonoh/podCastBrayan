dc2beb3bab8332afd562a52e8db8d823727482da
postonoh <sposton@emscorporate.com>
2020-03-20 16:07:08 -0400

    Fixt Tabbar and icon for progressbar

--- src/components/commons/TabBar.tsx
+++ src/components/commons/TabBar.tsx
@@ -7,6 +7,8 @@ import Player from './Player';
 import routes from '../../navigations/routes';
 import { metrics } from '../../constants/metrics';
 import { theme } from '../../constants/theme';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 
 
 const ICONS = {
@@ -16,29 +18,47 @@ const ICONS = {
     [routes.PROFILE]: 'user'
 }
 
-const activeTintColor = theme.color.blueDark;
-const inactiveTintColor = theme.color.redLight;
-
 
 const TabBar: React.FC<BottomTabBarProps> = (props) => {
     const insets = useSafeArea();
+
+    const activeTintColor = theme.color.blueDark;
+    const inactiveTintColor = theme.color.redLight;
+
+    const onTabPress = (routeName: string, routeIndex: number) => () => {
+        props.navigation.navigate(routeName);
+    };
+    
     return (
         <>
             <Player />
             <Box h={50 + insets.bottom} w="100%" dir="row">
                 {
-                    props.state.routes.map(route => {
+                    props.state.routes.map((route, index) => {
                         const icon = ICONS[route.name];
-                        const color = route.key === props.state.key ? activeTintColor : inactiveTintColor;
+
+                        const color = props.state.index === index ? activeTintColor : inactiveTintColor;
+
                         return (
-                            <Box key={route.key} f={1} center>
-                                <FeatherIcon name={icon} size={metrics.tabIconSize} color={color}/>
+                            <Box f={1} center key={route.key}>
+                                <TouchableOpacity style={styles.tabBtn}
+                                    onPress={onTabPress(route.name, index)}>
+                                    <Box f={1} center>
+                                        <FeatherIcon name={icon} size={metrics.tabIconSize} color={color} />
+                                    </Box>
+                                </TouchableOpacity>
                             </Box>
-                        )
+                        );
                     })}
             </Box>
         </>
     )
 }
-
+const styles = StyleSheet.create({
+    tabBtn: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 export default TabBar;

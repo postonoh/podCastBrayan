1c622096abcf9e0942cf9fa86cbae54d90041f55
postonoh <sposton@emscorporate.com>
2020-03-23 12:08:14 -0400

    Player design

--- src/components/commons/Player.tsx
+++ src/components/commons/Player.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import { Text, Box } from 'react-native-design-utility';
+import { Box, Text } from 'react-native-design-utility';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { observer } from 'mobx-react';
+
+import { metrics } from '../../constants/metrics';
+import { theme } from '../../constants/theme';
+import { useRootStore } from '../../contexts/RootStoreContext';
+
 
 const Player: React.FC = () => {
+    const { playerStore } = useRootStore();
+
+
     return (
-        <Box h={78} w="100%" bg="red">
+        <Box
+            h={70}
+            w="100%"
+            bg="redLighter"
+            dir="row"
+            align="center"
+            justify="between"
+            px="sm">
+            <Box dir="row" align="center" >
+                <Box w={50} h={50} radius={8} bg="white" mr="sm" />
+                <Text color="white" weight="bold">Learn React-Native</Text>
+            </Box>
+            <Box dir="row" f={1} align="center" justify="end">
+                <Box mr="sm">
+                    {playerStore.isPlaying ? (
+                        <TouchableOpacity hitSlop={metrics.makeHItSlop(20)}>
+                            <FeatherIcon color={theme?.color.white} name="pause" size={25} />
+                        </TouchableOpacity>
+                    ) : (
+                            <TouchableOpacity hitSlop={metrics.makeHItSlop(20)}>
+                                <FeatherIcon color={theme?.color.white} name="play" size={25} />
+                            </TouchableOpacity>
+                        )}
+                </Box>
+                <Box >
+                    <TouchableOpacity hitSlop={metrics.makeHItSlop(20)}>
+                        <FeatherIcon color={theme?.color.white} name="rotate-cw" size={25} />
+                    </TouchableOpacity>
+                </Box>
+
+            </Box>
+
         </Box>
+
     )
 }
 
-export default Player;
+export default observer(Player);
--- src/constants/metrics.tsx
+++ src/constants/metrics.tsx
@@ -1,3 +1,13 @@
+function makeHItSlop(size: number) {
+    return {
+        top: size,
+        right: size,
+        bottom: size,
+        left: size
+    }
+}
+
 export const metrics = {
     tabIconSize: 24,
+    makeHItSlop,
 };
